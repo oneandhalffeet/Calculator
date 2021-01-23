c75bf546d1c4bf05566bc0bb04c2b30ce826b577
Uddesh Teke <60895327+oneandhalffeet@users.noreply.github.com>
2021-01-23 10:24:07 +0530

    updated scanner for sys input and output
    The scanner has been added to input from the console and output the answer

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,19 @@ public class Main {
         executions[1] = create(200, 100, 's');
         executions[2] = create(300, 100, 'd');
         executions[3] = create(100, 10, 'm');
+        Scanner sc = new Scanner(System.in);
+        int i = 4;
+        while(sc.hasNext()){
+            int a,b;
+            char e;
+            a = sc.nextInt();
+            b = sc.nextInt();
+            c = sc.nextChar();
+            executions[i] = create(a, b, c);
+            i++;
+//             execution.execute();
+//             System.out.println("Result is:" + execution.getResult());
+        }
 
         for(MathExecution execution : executions){
             execution.execute();
